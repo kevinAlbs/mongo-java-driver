7a0904cb9139a74a3eb1e665cb4e5f6e692f304d
Jeff Yemin <jeff.yemin@10gen.com>
2015-07-27 13:47:30 -0400

    Fixed killCursors/killCursorsAsync Javadoc

--- driver-core/src/main/com/mongodb/connection/AsyncConnection.java
+++ driver-core/src/main/com/mongodb/connection/AsyncConnection.java
@@ -207,7 +207,7 @@ public interface AsyncConnection extends ReferenceCounted {
      *
      * @param cursors   the cursors
      * @param callback  the callback that is called once the cursors have been killed
-     * @deprecated Replace by {@link #killCursorAsync(MongoNamespace, List, SingleResultCallback)}
+     * @deprecated Replaced by {@link #killCursorAsync(MongoNamespace, List, SingleResultCallback)}
      */
     @Deprecated
     void killCursorAsync(List<Long> cursors, SingleResultCallback<Void> callback);
