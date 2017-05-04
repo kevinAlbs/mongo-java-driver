c46bc36697bf514f634fa81d08ae7981b1f1d6ad
Jeff Yemin <jeff.yemin@10gen.com>
2017-05-04 13:37:46 -0400

    JAVA-2507: When available (starting with Java 8), use java.time.format.DateFormatter.ISO_OFFSET_DATE_TIME instead of javax.xml.bind.DatatypeConverter.parseDateTime

--- bson/src/main/org/bson/json/JsonReader.java
+++ bson/src/main/org/bson/json/JsonReader.java
@@ -35,7 +35,6 @@ import org.bson.types.MaxKey;
 import org.bson.types.MinKey;
 import org.bson.types.ObjectId;
 
-import javax.xml.bind.DatatypeConverter;
 import java.text.DateFormat;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
@@ -988,7 +987,7 @@ public class JsonReader extends AbstractBsonReader {
             } else if (valueToken.getType() == JsonTokenType.STRING) {
                 String dateTimeString = valueToken.getValue(String.class);
                 try {
-                    value = DatatypeConverter.parseDateTime(dateTimeString).getTimeInMillis();
+                    value = DateTimeFormatter.parse(dateTimeString);
                 } catch (IllegalArgumentException e) {
                     throw new JsonParseException("Failed to parse string as a date", e);
                 }
