d0edf73bb25a0f584ce34eaf6f8505956278c490
Jeff Yemin <jeff.yemin@10gen.com>
2019-03-15 19:31:04 -0400

    Removed deprecated ConnectionString#getStreamType method
    Netty is now supported only via programmatic configuration of a
    NettyStreamFactoryFactory
    
    JAVA-3177

--- driver-core/src/test/unit/com/mongodb/ConnectionStringSpecification.groovy
+++ driver-core/src/test/unit/com/mongodb/ConnectionStringSpecification.groovy
@@ -159,7 +159,6 @@ class ConnectionStringSpecification extends Specification {
         connectionString.getServerSelectionTimeout() == 25000
         connectionString.getLocalThreshold() == 30
         connectionString.getHeartbeatFrequency() == 20000
-        connectionString.getStreamType() == 'netty'
         connectionString.getApplicationName() == 'app1'
 
         where:
@@ -167,7 +166,7 @@ class ConnectionStringSpecification extends Specification {
                 [new ConnectionString('mongodb://localhost/?minPoolSize=5&maxPoolSize=10&waitQueueMultiple=7&waitQueueTimeoutMS=150&'
                                             + 'maxIdleTimeMS=200&maxLifeTimeMS=300&replicaSet=test&'
                                             + 'connectTimeoutMS=2500&socketTimeoutMS=5500&'
-                                            + 'safe=false&w=1&wtimeout=2500&fsync=true&readPreference=primary&ssl=true&streamType=netty&'
+                                            + 'safe=false&w=1&wtimeout=2500&fsync=true&readPreference=primary&ssl=true&'
                                             + 'sslInvalidHostNameAllowed=true&'
                                             + 'serverSelectionTimeoutMS=25000&'
                                             + 'localThresholdMS=30&'
@@ -176,7 +175,7 @@ class ConnectionStringSpecification extends Specification {
                  new ConnectionString('mongodb://localhost/?minPoolSize=5;maxPoolSize=10;waitQueueMultiple=7;waitQueueTimeoutMS=150;'
                                             + 'maxIdleTimeMS=200;maxLifeTimeMS=300;replicaSet=test;'
                                             + 'connectTimeoutMS=2500;socketTimeoutMS=5500;'
-                                            + 'safe=false;w=1;wtimeout=2500;fsync=true;readPreference=primary;ssl=true;streamType=netty;'
+                                            + 'safe=false;w=1;wtimeout=2500;fsync=true;readPreference=primary;ssl=true;'
                                             + 'sslInvalidHostNameAllowed=true;'
                                             + 'serverSelectionTimeoutMS=25000;'
                                             + 'localThresholdMS=30;'
@@ -186,7 +185,7 @@ class ConnectionStringSpecification extends Specification {
                                             + 'maxIdleTimeMS=200&maxLifeTimeMS=300&replicaSet=test;'
                                             + 'connectTimeoutMS=2500;'
                                             + 'socketTimeoutMS=5500&'
-                                            + 'safe=false&w=1;wtimeout=2500;fsync=true&readPreference=primary;ssl=true&streamType=netty;'
+                                            + 'safe=false&w=1;wtimeout=2500;fsync=true&readPreference=primary;ssl=true;'
                                             + 'sslInvalidHostNameAllowed=true;'
                                             + 'serverSelectionTimeoutMS=25000&'
                                             + 'localThresholdMS=30;'
@@ -353,7 +352,6 @@ class ConnectionStringSpecification extends Specification {
         connectionString.getRequiredReplicaSetName() == null
         connectionString.getSslEnabled() == null
         connectionString.getSslInvalidHostnameAllowed() == null
-        connectionString.getStreamType() == null
         connectionString.getApplicationName() == null
         connectionString.getCompressorList() == []
         !connectionString.getRetryWrites()
