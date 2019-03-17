04985b7146d3c03782af3dc5692f4e850b726584
Jeff Yemin <jeff.yemin@10gen.com>
2019-03-17 13:36:07 -0400

    Remove unused classes in com.mongodb.operation package
    JAVA-3188

--- driver-core/src/test/functional/com/mongodb/internal/connection/ScramSha256AuthenticationSpecification.groovy
+++ driver-core/src/test/functional/com/mongodb/internal/connection/ScramSha256AuthenticationSpecification.groovy
@@ -26,16 +26,18 @@ import com.mongodb.binding.AsyncClusterBinding
 import com.mongodb.binding.ClusterBinding
 import com.mongodb.operation.CommandReadOperation
 import com.mongodb.operation.CommandWriteOperation
-import com.mongodb.operation.DropUserOperation
+import org.bson.BsonDocument
 import org.bson.BsonDocumentWrapper
+import org.bson.BsonString
 import org.bson.Document
+import org.bson.codecs.BsonDocumentCodec
 import org.bson.codecs.DocumentCodec
 import spock.lang.IgnoreIf
 import spock.lang.Specification
 
-import static com.mongodb.ClusterFixture.getBinding
 import static com.mongodb.ClusterFixture.createAsyncCluster
 import static com.mongodb.ClusterFixture.createCluster
+import static com.mongodb.ClusterFixture.getBinding
 import static com.mongodb.ClusterFixture.isAuthenticated
 import static com.mongodb.MongoCredential.createCredential
 import static com.mongodb.MongoCredential.createScramSha1Credential
@@ -90,7 +92,8 @@ class ScramSha256AuthenticationSpecification extends Specification {
     }
 
     def dropUser(final String userName) {
-        new DropUserOperation('admin', userName).execute(getBinding())
+        new CommandWriteOperation<>('admin', new BsonDocument('dropUser', new BsonString(userName)),
+            new BsonDocumentCodec()).execute(getBinding())
     }
 
     def 'test authentication and authorization'() {
